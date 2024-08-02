bdc8c66b709fe777f66bd4247552582defba3174
Laura Massonet <lamasson@1E3.42angouleme.fr>
2024-08-02 22:09:24 +0200

    bonus

---
+++ bonus/libasm_bonus.h
@@ -0,0 +1,14 @@
+# ifndef LIBASM_BONUS_H
+#define LIBASM_BONUS_H
+
+typedef struct  s_list
+{
+    void            *data;
+    struct s_list   *next;
+}t_list;
+
+int ft_atoi_base(char *str, char *base);
+void ft_list_push_front(t_list **node, void *data);
+int ft_list_size(t_list *begin_list);
+
+#endif
---
+++ bonus/main_bonus.c
@@ -0,0 +1,11 @@
+#include "libasm_bonus.h"
+
+int main(void) {
+    
+    t_list  *node;
+    char    *str = "Lol!";
+    
+    ft_list_push_front(t_list **node, void *str);
+
+    return (0);
+}
