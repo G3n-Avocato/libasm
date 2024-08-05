7331d53c33431fb68708022087f7e3014c22a35f
Laura Massonet <lamasson@1E2.42angouleme.fr>
2024-08-05 18:32:21 +0200

    bbb

--- bonus/ft_list_push_front.s
+++ bonus/ft_list_push_front.s
@@ -3,5 +3,10 @@ BITS 64
 section .text
     global ft_list_push_front
 
+; permet gestion propre de la pile, pour restaurer rbp
+; acces au parametre et var locales, creer ou manipulation de node rbp peut etre utiliser pour acceder facilement au element sans perturber la pile
+; convention d'appel 
 ft_list_push_front:
+    push rbp
+    mov rbp, rsp
     
